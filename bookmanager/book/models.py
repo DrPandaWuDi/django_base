68158f751b9f7cb671733a06298a2f93e8541dca
wzh <2025119707@qq.com>
2022-09-28 21:27:02 +0800

    创建模型类

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -6,9 +6,16 @@ from django.db import models
 class BookInfo(models.Model):
     name=models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 # 人物类
 class PeopleInfo(models.Model):
     name=models.CharField(max_length=10)
     gender=models.BooleanField()
     # 外键约束
+    # TypeError: __init__() missing 1 required positional argument: 'on_delete' 必须设置on_delete
     book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
